6cc1394321035999f9457db9e5cfe9d14d2094fd
snalmelwar <125942868+snalmelwar@users.noreply.github.com>
2023-04-26 22:03:43 -0400

    Delete lab3_stub directory

--- lab3_stub/helpers.js
+++
@@ -1,6 +0,0 @@
-//Todo You can use this file for any helper functions you may need. This file is optional and you don't have to use it if you do not want to.
-async function getMovies(url){
-    const { data } = await axios.get(url);
-    return data // this will be the array of user objects
-}
-
